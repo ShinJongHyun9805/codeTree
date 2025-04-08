58c24d7fd6cf225a529f42f952b8913aabf361b6
ShinJongHyun9805 <75189110+ShinJongHyun9805@users.noreply.github.com>
2025-04-08 22:36:15 +0900

    Title: 평균점수 구하기, Time: 251ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250408/평균점수 구하기/print-average.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        long count = Arrays.stream(str.split(" ")).mapToDouble(Double::valueOf).count();
+
+        double sum = Arrays.stream(str.split(" ")).mapToDouble(Double::valueOf).sum();
+        System.out.println(Math.round((sum / count) * 10.0) / 10.0);
+
+    }
+}
