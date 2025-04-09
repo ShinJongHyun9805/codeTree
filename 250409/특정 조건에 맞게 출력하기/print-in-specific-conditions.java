905b45086d7091aff806ad6f985deb73d1c0afcc
ShinJongHyun9805 <75189110+ShinJongHyun9805@users.noreply.github.com>
2025-04-09 16:10:59 +0900

    Title: 특정 조건에 맞게 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 250409/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
+++ 250409/특정 조건에 맞게 출력하기/print-in-specific-conditions.java
@@ -1,30 +1,24 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = Arrays.stream(sc.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) break;
+        StringBuilder sb = new StringBuilder();
+        while(st.hasMoreTokens()) {
 
-            list.add(arr[i]);
+            int num = Integer.parseInt(st.nextToken());
+            if (num == 0) break;
+
+            sb.append(num % 2 == 0 ? num / 2 : num + 3).append(" ");
         }
 
-        String str = list.stream()
-                .map(e -> (e % 2 == 0) ? e / 2 : e + 3)
-                .map(String::valueOf)
-                .collect(Collectors.joining(" "));
+        sb.setLength(sb.length() -1);
 
-        System.out.println(str);
+        System.out.println(sb);
     }
 }
